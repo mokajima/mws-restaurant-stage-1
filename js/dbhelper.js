c1302be5933e7e0412f89fc8d40c5ab6258113ca
mokajima <mokajima.85z@gmail.com>
2018-09-05 16:11:34 +0900

    refactor: Use fetch() to fetch all restaurants

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -22,19 +22,14 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch(DBHelper.DATABASE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        callback(null, data.restaurants);
+      })
+      .catch((e) => {
+        callback(e, null);
+      });
   }
 
   /**
